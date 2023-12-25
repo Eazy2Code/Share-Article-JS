9e8ed80ecfa99a394d45c1d2fc31eee7f1dc2e17
Eazy2Code <eazy2code786@gmail.com>
2023-12-24 23:27:04 -0800

    share article

---
+++ Share-Article-JS/script.js
@@ -0,0 +1,15 @@
+const input = document.getElementById("text");
+const copyButton = document.getElementById("copy");
+
+const copyText = (e) => {
+    input.select(); //select input value
+    document.execCommand("copy");
+    e.currentTarget.setAttribute("tooltip", "Copied!");
+};
+
+const resetTooltip = (e) => {
+    e.currentTarget.setAttribute("tooltip", "Copy to clipboard");
+};
+
+copyButton.addEventListener("click", (e) => copyText(e));
+copyButton.addEventListener("mouseover", (e) => resetTooltip(e));
